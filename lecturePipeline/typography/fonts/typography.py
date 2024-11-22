5aea679b3dc20d0eb30ff9d22e4e22e49d9936f2
Kid A <thisismeamir@outlook.com>
2024-11-22 21:16:42 +0330

    assets

---
+++ lecturePipeline/typography/fonts/typography.py
@@ -0,0 +1,34 @@
+import yaml
+import yaml
+from typing import Any, Optional
+
+def typography(key: str, default: Any = None):
+        """
+        Initialize CourseConfig with the path to the YAML file.
+
+        :param file_path: Path to the YAML configuration file.
+        """
+        file_path = "typography.yml"
+        try:
+            with open(file_path, 'r') as file:
+                data = yaml.safe_load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {file_path}")
+        except yaml.YAMLError as e:
+            raise ValueError(f"Error parsing YAML file: {e}")
+        keys = key.split('.')
+        value = data
+
+        for k in keys:
+            if isinstance(value, dict):
+                value = value.get(k)
+            else:
+                return default
+
+            if value is None:
+                return default
+
+        return value
+
+if __name__ == "__main__":
+    print(typography("fluid.display-01-mx"))
